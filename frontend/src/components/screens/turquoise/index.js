42cc1be5d6c0e914f9778a39ebd5673cefef7ad8
Jerge <erikjer@student.chalmers.se>
2018-12-10 13:58:51 +0100

    colour and shit talk on rest of P.R.I.T.

--- frontend/src/components/screens/turquoise/index.js
+++ frontend/src/components/screens/turquoise/index.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { TurquoiseContainer } from './styles'
+import { TurquoiseContainer } from "./styles";
 
-class TurquoiseScreen extends Component{
-    render() {
-        return (
-            <TurquoiseContainer>
-                Gör revolution! Fuck babyblå, Make Turkos Turkos again! #00dab0
-            </TurquoiseContainer>
-        )
-    }
+class TurquoiseScreen extends Component {
+  render() {
+    return (
+      <TurquoiseContainer>
+        Gör revolution! Fuck babyblå, Make Turkos Turkos again! #00babe
+      </TurquoiseContainer>
+    );
+  }
 }
 
 export default TurquoiseScreen;
--- frontend/src/components/views/members/index.js
+++ frontend/src/components/views/members/index.js
@@ -1,74 +1,126 @@
-import React from 'react'
+import React from "react";
 
-import { MemberContainer, MemberTempContainer, TempMemberImage, ChefChefImage, Name, Text } from './styles';
+import {
+  /*MemberContainer,*/
+  MemberTempContainer,
+  TempMemberImage,
+  /*ChefChefImage,*/
+  Name,
+  Text
+} from "./styles";
 // import { DigitDesign } from '@cthit/react-digit-components'
 
-export const TempMemberContent = () => 
-    <MemberTempContainer>
-        <TempMemberImage/>
-        <Name>ChefChef: Alexander “Semlan” Selmanovic</Name>
-        <Name>Ka$$Chef: Jian “Finn” Shin</Name>
-        <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
-        <Name>MaterialChef: Sara “Kitzing” Kitzing</Name>
-        <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
-        <Name>MatChef : Erik “Jerge” Jergéus</Name>
-        <Name>BrystChef: Filip "Ananas" Andréasson</Name>
-        <br/><br/>
-        <Name>Mer info kommer senare!</Name>
-    </MemberTempContainer>
-
-export const ChefChefContent = () => 
-    <MemberContainer>
-        {/* <DigitDesign.Card></DigitDesign.Card> */}
-        <ChefChefImage/>
-        <Name>ChefChef: Alexander “Semlan” Selmanovic</Name>
-        <Name>Ka$$Chef: Jian “Finn” Shin</Name>
-        <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
-        <Name>MaterialChef: Sara “Kitzing” Kitzing</Name>
-        <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
-        <Name>MatChef : Erik “Jerge” Jergéus</Name>
-        <Name>BrystChef: Filip "Ananas" Andréasson</Name>
-        <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-    </MemberContainer>
-
-export const KassChefContent = () => 
-    <MemberContainer>
-        <ChefChefImage/>
-        <Name>Ka$$Chef: Jian “Finn” Shin</Name>
-        <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-    </MemberContainer>
-
-export const OhlChefContent = () => 
-    <MemberContainer>
-        <ChefChefImage/>
-        <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
-        <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-    </MemberContainer>
-
-export const MaterialChefContent = () => 
-<MemberContainer>
-    <ChefChefImage/>
+export const TempMemberContent = () => (
+  <MemberTempContainer>
+    <TempMemberImage />
+    <Name>ChefChef: Alexander “Semlan” Selmanovic</Name>
+    <Name>Ka$$Chef: Jian “Finn” Shin</Name>
+    <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
     <Name>MaterialChef: Sara “Kitzing” Kitzing</Name>
-    <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-</MemberContainer>
-
-export const PRChefContent = () => 
-    <MemberContainer>
-        <ChefChefImage/>
-        <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
-        <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-    </MemberContainer>
-
-export const MatChefContent = () => 
-<MemberContainer>
-    <ChefChefImage/>
+    <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
     <Name>MatChef : Erik “Jerge” Jergéus</Name>
-    <Text>Hälsninar, det är jag som är Jerge, P.R.I.T.'s MatChef! Som MatChef fixar jag göttigt käk till pubrundorna</Text>
-</MemberContainer>
+    <Name>BrystChef: Filip "Ananas" Andréasson</Name>
+    <br />
+    <br />
+    <Text>
+      Mer info kommer senare (förmodligen aldrig för folk skickade inte in sin
+      beskrivning //sad MatChef)!
+    </Text>
+  </MemberTempContainer>
+);
+/*
+export const ChefChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>ChefChef: Alexander “Semlan” Selmanovic</Name>
+    <Name>Ka$$Chef: Jian “Finn” Shin</Name>
+    <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
+    <Name>MaterialChef: Sara “Kitzing” Kitzing</Name>
+    <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
+    <Name>MatChef : Erik “Jerge” Jergéus</Name>
+    <Name>BrystChef: Filip "Ananas" Andréasson</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
 
-export const BryChefContent = () => 
-    <MemberContainer>
-        <ChefChefImage/>
-        <Name>BrystChef: Filip "Ananas" Andréasson</Name>
-        <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </Text>
-    </MemberContainer>
+export const KassChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>Ka$$Chef: Jian “Finn” Shin</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
+
+export const OhlChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>ØhlChef: Mathias “Lambi” Lammers</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
+
+export const MaterialChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>MaterialChef: Sara “Kitzing” Kitzing</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
+
+export const PRChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>PR-Chef: Camilla “Spurt” Södelund</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
+
+export const MatChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>MatChef : Erik “Jerge” Jergéus</Name>
+    <Text>
+      Hälsninar, det är jag som är Jerge, P.R.I.T.'s MatChef! Som MatChef fixar
+      jag göttigt käk till pubrundorna
+    </Text>
+  </MemberContainer>
+);
+
+export const BryChefContent = () => (
+  <MemberContainer>
+    <ChefChefImage />
+    <Name>BrystChef: Filip "Ananas" Andréasson</Name>
+    <Text>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+    </Text>
+  </MemberContainer>
+);
+*/
